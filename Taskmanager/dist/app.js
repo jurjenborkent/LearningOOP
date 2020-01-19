4bccaa207f4c2b2270797b5e128628bb1c3ca985
Jurjen Borkent <jurjenborkent@gmail.com>
2020-01-19 12:07:24 +0100

    More logical namegiving
    More logical namegiving

---
+++ Taskmanager/dist/app.js
@@ -0,0 +1,126 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function validate(validatableInput) {
+    let isValid = true;
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
+        isValid = isValid && validatableInput.value.length > validatableInput.minLength;
+    }
+    if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+        isValid = isValid && validatableInput.value.length < validatableInput.maxLength;
+    }
+    if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+        isValid = isValid && validatableInput.value > validatableInput.min;
+    }
+    if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+        isValid = isValid && validatableInput.value < validatableInput.max;
+    }
+    return isValid;
+}
+function autoBind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDecriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDecriptor;
+}
+class TaskInputForm {
+    constructor() {
+        this.templateElement = document.getElementById('task-input');
+        this.hostElement = document.getElementById('app');
+        const importedHtmlContent = document.importNode(this.templateElement.content, true);
+        this.element = importedHtmlContent.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.pointsInputElement = this.element.querySelector('#points');
+        this.configure();
+        this.attach();
+    }
+    gatherFormInput() {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPoints = this.pointsInputElement.value;
+        const titleValidatable = {
+            value: enteredTitle,
+            required: true,
+        };
+        const descriptionValidatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+        };
+        const pointsValidatable = {
+            value: +enteredPoints,
+            required: true,
+            min: 1,
+            max: 5
+        };
+        if (!validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(pointsValidatable)) {
+            alert('Vul het formulier goed in');
+            return;
+        }
+        else {
+            return [enteredTitle, enteredDescription, +enteredPoints];
+        }
+    }
+    clearForm() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.pointsInputElement.value = '';
+    }
+    submitHandler(event) {
+        event.preventDefault();
+        const formInput = this.gatherFormInput();
+        if (Array.isArray(formInput)) {
+            const [title, desc, people] = formInput;
+            console.log(title, desc, people);
+            this.clearForm();
+        }
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler);
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+__decorate([
+    autoBind
+], TaskInputForm.prototype, "submitHandler", null);
+class TaskList {
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById('task-list');
+        this.hostElement = document.getElementById('app');
+        const importedHtmlContent = document.importNode(this.templateElement.content, true);
+        this.element = importedHtmlContent.firstElementChild;
+        this.element.id = `${type}-tasks`;
+        this.attach();
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-tasks-list`;
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent = this.type.toUpperCase() + ' TAKEN';
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
+const prjInput = new TaskInputForm();
+const activatePrjList = new TaskList('active');
+const finishedPrjList = new TaskList('finished');
---
+++ Taskmanager/src/app.ts
@@ -0,0 +1,175 @@
+// validation voor het input veld
+
+interface Validator {
+  value: string | number
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validator) {
+
+  // hier voeren we een check uit op voorwaarden die gelden bij het invullen van velden.
+
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0
+  }
+  if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
+    isValid = isValid && validatableInput.value.length > validatableInput.minLength;
+  }
+
+  if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+    isValid = isValid && validatableInput.value.length < validatableInput.maxLength;
+  }
+
+  if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+    isValid = isValid && validatableInput.value > validatableInput.min;
+  }
+
+  if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+    isValid = isValid && validatableInput.value < validatableInput.max;
+  }
+
+  return isValid
+}
+
+
+// autoBind decorator 
+
+function autoBind(
+  _: any,
+  _2: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDecriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  };
+  return adjDecriptor;
+}
+
+class TaskInputForm {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  pointsInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = document.getElementById('task-input')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedHtmlContent = document.importNode(this.templateElement.content, true);
+    this.element = importedHtmlContent.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.pointsInputElement = this.element.querySelector('#points') as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  private gatherFormInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPoints = this.pointsInputElement.value;
+
+    const titleValidatable: Validator = {
+      value: enteredTitle,
+      required: true,
+    };
+
+    const descriptionValidatable: Validator = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5
+    };
+
+    const pointsValidatable: Validator = {
+      value: +enteredPoints,
+      required: true,
+      min: 1,
+      max: 5
+    };
+
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(pointsValidatable)
+    ) {
+      alert('Vul het formulier goed in');
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPoints];
+    }
+  }
+
+  private clearForm() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.pointsInputElement.value = '';
+  }
+
+  @autoBind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const formInput = this.gatherFormInput();
+    if (Array.isArray(formInput)) {
+      const [title, desc, people] = formInput;
+      console.log(title, desc, people)
+      this.clearForm();
+    }
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler);
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+// project lijst class 
+
+class TaskList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById('task-list')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedHtmlContent = document.importNode(this.templateElement.content, true);
+    this.element = importedHtmlContent.firstElementChild as HTMLElement;
+    this.element.id = `${type}-tasks`
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-tasks-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' TAKEN';
+  }
+
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
+}
+
+const prjInput = new TaskInputForm();
+const activatePrjList = new TaskList('active');
+const finishedPrjList = new TaskList('finished');
